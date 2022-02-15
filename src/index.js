70dded6b9bdba4b5a77037ab273989a6934cc047
Md Ruhin Mia <radiustheme2@gmail.com>
2022-02-15 15:46:04 +0600

    props with functional component

--- src/index.js
+++ src/index.js
@@ -1,31 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// const content = () => {
-//   return (
-//     <div>
-//       <h1>Hello world</h1>
 
-
-//     </div >
-
-
-//   )
-// }
-
-// const content = <h1>Hello React</h1>
-
-const Content = () => {
-  const [counter, setCounter] = React.useState(0)
+const Content = ({ locale }) => {
   return (
-
-    <div> <h1>{counter}</h1>
-      <button id="submitBtn" onClick={() => setCounter(counter + 1)}>Click</button>
-
-    </div>
+    <h1 className="header">Time {new Date().toLocaleTimeString(locale)}</h1>
   )
-
 }
-
-// ReactDOM.render(content(), document.getElementById('root'))
-ReactDOM.render(<div><Content /><Content /></div>, document.getElementById('root'))
+ReactDOM.render(<Content locale="bn-BD" />, document.getElementById('root'))
